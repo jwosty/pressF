f132b5e2ae999dfae37d55b9840393009a4f4018
John Wostenberg <jwosty@users.noreply.github.com>
2022-04-28 16:44:42 -0600

    Fix bukkit version of the plugin again

--- pressF-bukkit/src/main/java/com/wostenberg/minecraft/pressf/bukkit/PressFPlugin.java
+++ pressF-bukkit/src/main/java/com/wostenberg/minecraft/pressf/bukkit/PressFPlugin.java
@@ -17,42 +17,31 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.wostenberg.minecraft.pressf.PressFCore;
 
 public class PressFPlugin extends JavaPlugin implements Listener {
-    // FileConfiguration config;
-    
-//    static final String discordWebhookUrlPath = "discordWebhookUrl";
-//    static final String enabledPath = "enabled";
-
     PressFCore corePlugin;
 
     @Override
     public void onEnable() {
         corePlugin = new PressFCore(getSLF4JLogger(), getDataFolder().toPath().resolve("config.yml"));
 
-        this.loadConfig();
-        this.saveConfig();
+        corePlugin.loadConfig();
+        corePlugin.saveConfig();
 
         this.getServer().getPluginManager().registerEvents(this, this);
-        CommandReload ex = new CommandReload(getLogger(), () -> loadConfig());
+        CommandReload ex = new CommandReload(getLogger(), () -> corePlugin.loadConfig());
         this.getCommand("reload").setExecutor(ex);
     }
 
+    @Override
+    public void saveConfig() {
+        corePlugin.saveConfig();
+    }
+
     public void loadConfig() {
-//        super.reloadConfig();
-//        config = this.getConfig();
-//        config.addDefault(discordWebhookUrlPath, "https://discordapp.com/api/webhooks/some/webhook");
-//        config.addDefault(enabledPath, true);
-//        config.options().copyDefaults(true);
         corePlugin.loadConfig();
     }
 
     @EventHandler
     public void onDeath(PlayerDeathEvent event) throws IOException {
-//        boolean enabled = config.getBoolean(enabledPath);
-//        if (enabled) {
-//            String deathMsg = event.getDeathMessage();
-//            String strUrl = config.getString(discordWebhookUrlPath);
-//            corePlugin.onDeath(deathMsg);
-//        }
         corePlugin.onDeath(event.getDeathMessage());
     }
 }
